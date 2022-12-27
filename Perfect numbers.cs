b424e2df3480602aaf3e15f5c8633db577f7715d
Noran-cloud <121050473+Noran-cloud@users.noreply.github.com>
2022-12-27 20:56:40 +0200

    Add files via upload

---
+++ Perfect numbers.cs
@@ -0,0 +1,30 @@
+﻿// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class Perfect_Numbers
+{
+    public static void Main(string[] args)
+    {
+       Console.WriteLine("Please,enter first number ");
+       int num1 = int.Parse(Console.ReadLine());
+       
+       Console.WriteLine("Please,enter last number ");
+       int num2 = int.Parse(Console.ReadLine());
+       
+       Console.WriteLine("Perfect numbers between them are :");
+       
+       for (int i = num1; i <= num2; i++)
+       {
+           int sum = 0;
+          for (int j = 1; j < i; j++)
+          {
+              if (i % j == 0)
+              sum+= j;
+          }
+          if (sum == i)
+         Console.Write("{0},",i);
+       }
+    }
+}
